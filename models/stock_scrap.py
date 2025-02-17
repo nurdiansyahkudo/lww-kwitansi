00b0385cd943b95b81b98194ac4ad5a014f16db8
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-02-17 10:12:01 +0700

    Add Scrap Multiple Lot/SN

--- models/stock_scrap.py
+++ models/stock_scrap.py
@@ -4,7 +4,7 @@ class StockScrap(models.Model):
     _inherit = "stock.scrap"
 
     lot_ids = fields.Many2many(
-        'stock.lot', 'Lot/Serial',
+        'stock.lot', string='Lots/Serials',
         domain="[('product_id', '=', product_id), ('product_qty', '>', 0)]", 
         check_company=True
     )
