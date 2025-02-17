101e43cc7f6dd4602fd567e205a3864e5366b517
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-02-17 10:38:01 +0700

    Add Scrap Multiple Lot/SN

--- models/stock_scrap.py
+++ models/stock_scrap.py
@@ -8,3 +8,26 @@ class StockScrap(models.Model):
         domain="[('product_id', '=', product_id), ('product_qty', '>', 0)]", 
         check_company=True
     )
+    scrap_qty = fields.Float(
+        'Quantity', required=True, digits='Product Unit of Measure',
+        compute='_compute_scrap_qty', default=1.0, readonly=False, store=True
+    )
+
+    @api.depends('lot_ids.quantity')  # Mendengar perubahan pada lot_ids
+    def _compute_scrap_qty(self):
+        for scrap in self:
+            if scrap.lot_ids:
+                # Hitung total quantity berdasarkan lot yang dipilih
+                total_qty = sum(lot.quantity for lot in scrap.lot_ids)
+                scrap.scrap_qty = total_qty
+            else:
+                scrap.scrap_qty = 0
+
+    @api.onchange('lot_ids')  # Menangani perubahan pada lot_ids
+    def _onchange_lot_ids(self):
+        if self.lot_ids:
+            # Hitung total quantity berdasarkan lot yang dipilih
+            total_qty = sum(lot.quantity for lot in self.lot_ids)
+            self.scrap_qty = total_qty
+        else:
+            self.scrap_qty = 0
