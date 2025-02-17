9da28f8f1813221e8daa6c98ac07d307d385beb0
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-02-17 09:22:21 +0700

    Add Scrap Multiple Lot/SN

--- models/stock_scrap.py
+++ models/stock_scrap.py
@@ -1,10 +1,18 @@
 from odoo import models, fields, api
 
-class StockPicking(models.Model):
-    _inherit = "stock.scrap"
+class StockScrapLot(models.Model):
+    _name = "stock.scrap.lot"
+    _description = "Stock Scrap Lot"
 
-    lot_id = fields.Many2one(
-        'stock.lot', 'Lot/Serial',
-        domain="[('product_id', '=', product_id), ('product_qty', '>', 0)]", 
-        check_company=True
-    )
+    scrap_id = fields.Many2one('stock.scrap',
+                                string="Scrap Reference",
+                                required=True,
+                                ondelete="cascade"
+                               )
+    lot_id = fields.Many2one('stock.lot', string='Lot/Serial', required=True, 
+                             domain="[('product_id', '=', product_id), ('product_qty', '>', 0)]")
+    quantity = fields.Float(string='Quantity', required=True, default=1.0)
+class StockScrap(models.Model):
+    _inherit = 'stock.scrap'
+
+    scrap_lot_ids = fields.One2many('stock.scrap.lot', 'scrap_id', string='Lot/Serial Numbers')
