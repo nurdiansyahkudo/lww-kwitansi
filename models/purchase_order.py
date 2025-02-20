d2096438cb7cfad72efd72cd376694ef99c3f442
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-02-20 16:44:17 +0700

    Change the default Numbering in PO

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,22 +1,17 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-from num2words import num2words
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    name = fields.Char('Order Reference', 
-        required=True, 
-        index='trigram', 
-        copy=False, 
-        default=lambda self: self._get_next_sequence()
+    name = fields.Char(
+        'Order Reference',
+        required=True,
+        index='trigram',
+        copy=False,
+        default=lambda self: self._default_get_name()
     )
     no_po = fields.Char(string='Order Number', store=True, required=True)
-
-    @api.model
-    def _get_next_sequence(self):
-        """ Generate the next Sequence when click New """
-        return self.env['ir.sequence'].next_by_code('purchase.order') or 'New'
 
     def action_print_report(self):
         company = self.env['res.company'].browse(self.env.company.id)
@@ -26,19 +21,39 @@ class PurchaseOrder(models.Model):
         elif company.name == 'PT. SPARTADUA RIBUJAYA':
             return self.env.ref('lww_kwitansi.action_report_spartadua_po').report_action(self)
         else:
-            # Jika perusahaan tidak cocok dengan ketiganya, menggunakan laporan default
             return self.env.ref('lww_kwitansi.action_report_limawira_po').report_action(self)
+
+    @api.model
+    def _default_get_name(self):
+        """ Generate sequence only when form is opened (not in create) """
+        return self.env['ir.sequence'].next_by_code('purchase.order') or '/'
 
     @api.model_create_multi
     def create(self, vals_list):
+        orders = self.browse()
+        partner_vals_list = []
         for vals in vals_list:
+            # Jika name masih kosong (tidak diisi di form), pastikan tetap pakai sequence
+            if not vals.get('name'):
+                vals['name'] = self.env['ir.sequence'].next_by_code('purchase.order') or '/'
+
+            # Cek jika no_po sudah ada
             if 'no_po' in vals and vals['no_po']:
                 existing_record = self.env['purchase.order'].search([
                     ('no_po', '=', vals['no_po'])
                 ], limit=1)
                 if existing_record:
                     raise ValidationError('PO Sudah Ada!')
-        return super().create(vals_list)
+
+            vals, partner_vals = self._write_partner_values(vals)
+            partner_vals_list.append(partner_vals)
+            orders |= super(PurchaseOrder, self).create(vals)
+
+        for order, partner_vals in zip(orders, partner_vals_list):
+            if partner_vals:
+                order.sudo().write(partner_vals)
+
+        return orders
 
     def write(self, vals):
         if 'no_po' in vals and vals['no_po']:
