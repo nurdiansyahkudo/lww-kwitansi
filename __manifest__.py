70a900f8fb27881e1a715bed4162cf568f5a3085
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-03-12 11:43:13 +0700

    Update new Kwitansi

--- __manifest__.py
+++ __manifest__.py
@@ -12,26 +12,8 @@
         'views/lww_kwitansi_template.xml',
         'views/bs_kwitansi_template.xml',
         'views/spartadua_kwitansi_template.xml',
-        # SALES ORDER
-        'report/lww_sales_report.xml',
-        'views/lww_so_template.xml',
-        # 'views/bs_so_template.xml',
-        # 'views/spartadua_so_template.xml',
-        # RECEIPT GOODS
-        'report/lww_receipt_report.xml',
-        'views/lww_receipt_template.xml',
-        'views/bs_receipt_template.xml',
-        # 'views/spartadua_receipt_template.xml',
-        # DELIVERY ORDER
-        'report/lww_do_report.xml',
-        'views/lww_do_template.xml',
-        'views/bs_do_template.xml',
-        'views/spartadua_do_template.xml',
         # FORM VIEW
         'views/account_payment_view.xml',
-        'views/sale_order_view.xml',
-        'views/stock_picking_view.xml',
-        'views/stock_scrap_view.xml',
         # 'views/header_template.xml',
     ],
     'installable': True,
